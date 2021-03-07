94b58413d9291221d3eee22cfbec55ff0245197a
JOY_91 <565760450@qq.com>
2021-03-07 22:57:15 +0800

    crawler中引入多线程

---
+++ python练习/13进程和线程/crawler_thread.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import urllib
+from bs4 import BeautifulSoup
+import threading
+from threading import Thread
+import time
+import requests
+import json
+import time
+
+class DownloadHanlder(Thread):
+
+    def __init__(self, url, q):
+        super(DownloadHanlder, self).__init__()
+        self.url = url
+        self.q = q
+
+    def run(self):
+        result = urllib.urlopen(self.url)
+        r = result.read()
+        title = BeautifulSoup(r, "html.parser").find('header', 'title').find('h1').get_text()
+        print "title" + title
+        content = BeautifulSoup(r, "html.parser").find('div', 'cnbeta-article-body').find_all('p')
+        for texts in content:
+            p = texts.get_text()
+            # print p
+        # cursor = connection.cursor()
+        # cursor.execute('insert into cnbeta_information (info_url,info_title,info_content) value (%s,%s,%s)') % [hrefs, title, p]
+
+def addCnbeta():
+    html = urllib.urlopen('https://www.cnbeta.com/')   # 抓取网页的源码
+    response = html.read()  # 读取文件
+    res = BeautifulSoup(response, "html.parser").find_all('a')
+    hrefList = []
+    for text in res:
+        href = text.get('href')
+        if href == None:
+            continue
+        elif 'articles' not in href:
+            continue
+        elif href.startswith('//'):
+            href = 'http:' + href
+        hrefList.append(href)
+    hrefList = set(hrefList)  # set中的元素是无序的，并且重复元素在set中自动被过滤
+    hrefList=list(hrefList)
+    hrefList.sort()
+    html.close()
+    print len(hrefList)
+
+    start=time.time()
+    threads=[]
+    for hrefs in hrefList:
+        t=DownloadHanlder(hrefs,queue)
+        t.start()
+        threads.append(t)
+        t.join()
+    print len(threading.enumerate())
+    end=time.time()
+    print 'Execution time: ', (end - start), 's'
+
+
+if __name__ == '__main__':
+    addCnbeta()
