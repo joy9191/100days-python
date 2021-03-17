ae2d2549b99168a142e88b5a5cfe7a97c12d7e11
joy9191 <565760450@qq.com>
2021-03-17 15:58:52 +0800

    add email

--- python练习/14网络应用开发/smtp_email.py
+++ python练习/14网络应用开发/smtp_email.py
@@ -8,7 +8,8 @@ from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
 
-def main():
+def textfiles():
+	# 发送纯文本邮件
     # 请自行修改下面的邮件发送者和接收者
     sender = '565760450@qq.com'
     receivers = ['jiuyi@mgtv.com','zhigoo.wang@gmail.com']
@@ -22,28 +23,70 @@ def main():
     smtper.sendmail(sender, receivers, message.as_string())
     print('邮件发送完成!')
 
-def imagefile():
+def imagefiles():
+	# 发送带附件的邮件
     sender = '565760450@qq.com'
-    receivers = 'jiuyi@mgtv.com;zhigoo.wang@gmail.com'
-    message = MIMEMultipart()
+    receivers = ['jiuyi@mgtv.com','2298058405@qq.com']
+    message = MIMEMultipart('mixed')
 
     # message['From'] = Header('周泥煤', 'utf-8')
     # message['To'] = Header('王大锤', 'utf-8')
     message['From'] = sender
-    message['To'] = receivers
-    message['Subject'] = Header('Python发送邮件测试', 'utf-8')
+    message['To'] = ','.join(receivers)
+    message['Subject'] = 'Python发送带附件的邮件测试'
 
-    with open('/Users/zhoujiuyi/Desktop/20200820.jpeg','rb') as fp:
+    print(message)
+
+    # with open('/Users/zhoujiuyi/Desktop/20200820.jpeg','rb') as fp:
+    with open('C:/Users/KLYG/Desktop/12590.jpg','rb') as fp:	
         msgImage = MIMEImage(fp.read())
     msgImage.add_header('Content-ID','imgid') #为图片对象拓展标题字段和值
     message.attach(msgImage)
 
-    smtper = SMTP('smtp.qq.com')
-    # 请自行修改下面的登录口令
-    smtper.login(sender, 'xzbdewappnvwbbca')
-    smtper.sendmail(sender, receivers, message.as_string())
-    print('邮件发送完成!')
+    text = "以上为自动发送邮件\n有任何问题请联系我"
+    text_plain = MIMEText(text,'plain', 'utf-8')
+    message.attach(text_plain)
+
+    try:
+    	smtper = SMTP('smtp.qq.com')
+    	# 发送方邮箱授权码
+    	smtper.login(sender, 'xzbdewappnvwbbca')
+    	smtper.sendmail(sender, message['To'].split(','), message.as_string())
+    	print('邮件发送完成!')
+    except smtplib.SMTPException as e:
+    	print('ERROR:无法发送邮件.Case:%s'%e)
+
+def htmlfiles():
+	sender = '565760450@qq.com'
+	receivers = ['jiuyi@mgtv.com','2298058405@qq.com']
+	message = MIMEMultipart('mixed')
+
+	message['From'] = sender
+	message['To'] = ','.join(receivers)
+	message['Subject'] = 'Python发送HTML邮件测试'
+	# with open('C:/Users/KLYG/Desktop/12590.jpg','rb') as fp:
+	# 	msgImage = MIMEImage(fp.read())
+
+	fp=open('C:/Users/KLYG/Desktop/12590.jpg','rb')
+	msgImage=MIMEImage(fp.read())
+	fp.close()
+	msgImage.add_header('Content-ID','<img1>')
+	message.attach(msgImage)
+
+	content= '<h1>Hello</h1>' + '<p>send by <a href="http://www.python.org">Python</a>...</p>' + '<img src="cid:img1" alt="书房" />'
+
+	htm=MIMEText(content, 'html', 'utf-8')
+	message.attach(htm)
+
+	try:
+		smtper = SMTP('smtp.qq.com')
+		# 发送方邮箱授权码
+		smtper.login(sender, 'xzbdewappnvwbbca')
+		smtper.sendmail(sender, message['To'].split(','), message.as_string())
+		print('邮件发送完成!')
+	except smtplib.SMTPException as e:
+		print('ERROR:无法发送邮件.Case:%s'%e)
 
 
 if __name__ == '__main__':
-    imagefile()
+    htmlfiles()
