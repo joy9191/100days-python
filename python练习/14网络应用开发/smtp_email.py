97c95449d0fe9ca1172279b0f025c09e6a782fb4
joy9191 <565760450@qq.com>
2021-03-18 12:04:37 +0800

    add email tmp

--- python练习/14网络应用开发/smtp_email.py
+++ python练习/14网络应用开发/smtp_email.py
@@ -10,9 +10,8 @@ from email.mime.multipart import MIMEMultipart
 
 def textfiles():
 	# 发送纯文本邮件
-    # 请自行修改下面的邮件发送者和接收者
     sender = '565760450@qq.com'
-    receivers = ['jiuyi@mgtv.com','zhigoo.wang@gmail.com']
+    receivers = ['jiuyi@mgtv.com']
     message = MIMEText('用Python发送邮件的示例代码.', 'plain', 'utf-8')
     message['From'] = Header('周泥煤', 'utf-8')
     message['To'] = Header('王大锤', 'utf-8')
@@ -89,4 +88,4 @@ def htmlfiles():
 
 
 if __name__ == '__main__':
-    htmlfiles()
+    textfiles()
