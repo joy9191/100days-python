6d4cd3cd86b9b43d73f210cfc82bdea5e8f5ff9f
joy9191 <565760450@qq.com>
2021-05-06 18:20:12 +0800

    嵌套列表

--- python练习/16列表生成式/nest.py
+++ python练习/16列表生成式/nest.py
@@ -5,14 +5,14 @@ names = ['关羽', '张飞', '赵云', '马超', '黄忠']
 courses = ['语文', '数学', '英语']
 # 录入五个学生三门课程的成绩
 # 错误 - 参考http://pythontutor.com/visualize.html#mode=edit
-scores = [[None] * len(courses)] * len(names)
-print(scores)
-# scores = [[None] * len(courses) for _ in range(len(names))]
+# scores = [[None] * len(courses)] * len(names)
 # print(scores)
+scores = [[None] * len(courses) for _ in range(len(names))]
+print(scores)
 for row, name in enumerate(names):
     for col, course in enumerate(courses):
-    	print(row,col)
     	scores[row][col] = float(input(f'请输入{name}的{course}成绩: '))
+    	print(scores)
     	print(id(scores[0]))
     	print(id(scores[1]))
 
