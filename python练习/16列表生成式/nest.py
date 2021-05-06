d72c903396e42d61a54f14b7235f4ea92a1d5b1a
joy9191 <565760450@qq.com>
2021-05-06 18:13:49 +0800

    嵌套列表

--- python练习/16列表生成式/nest.py
+++ python练习/16列表生成式/nest.py
@@ -13,7 +13,8 @@ for row, name in enumerate(names):
     for col, course in enumerate(courses):
     	print(row,col)
     	scores[row][col] = float(input(f'请输入{name}的{course}成绩: '))
-    	print(scores)
+    	print(id(scores[0]))
+    	print(id(scores[1]))
 
 
 # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标，一般用在 for 循环当中
