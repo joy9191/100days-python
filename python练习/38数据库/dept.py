ba0e84e595a3f1ab2cfa1edae1e0f0c9d116ca13
JOY_91 <565760450@qq.com>
2021-08-31 22:53:45 +0800

    20210831

--- python练习/38数据库/dept.py
+++ python练习/38数据库/dept.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 #coding=utf-8
-import MySQLdb
+import pymysql
 
-def main():
+def addDept():
 	# 添加部门数据表
 	no = int(input('编号: '))
 	name = raw_input('名字: ')
 	loc = raw_input('所在地: ')
-	con = MySQLdb.connect(host='localhost', port=3306,
+	con = pymysql.connect(host='localhost', port=3306,
                           database='cnbeta', charset='utf8',
-                          user='root', password='joy890621')
+                          user='root', password='joy890621',
+                          autocommit =True)
 	try:
 		# 使用with as语句操作上下文管理器，作用是确保数据库使用完后会关闭连接
     	# 使用with as无论期间是否抛出异常，都能保证 with as 语句执行完毕后自动关闭已经打开的文件
@@ -22,9 +23,27 @@ def main():
         	if result==1:
         		print('添加成功!')
         # 4. 操作成功提交事务
-        	con.commit()
+        	# con.commit()
         finally:
         	con.close()
 
+def delDept():
+	no = int(input('要删除的部门编号: '))
+	con = pymysql.connect(host='localhost', port=3306,
+                          database='cnbeta', charset='utf8',
+                          user='root', password='joy890621',
+                          autocommit =True)
+	try:
+		with con.cursor() as cursor:
+			result = cursor.execute(
+				'delete from tb_dept where dno=%s',no
+				)
+			print(result)
+			if result==1:
+				print('没有该数据，删除成功！')
+	finally:
+		con.close()
+
 if __name__ == '__main__':
-	main()
+	addDept()
+	delDept()
