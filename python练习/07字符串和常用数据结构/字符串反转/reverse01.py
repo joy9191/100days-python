5d8d23898a25e3a266ee61bc6365f08871c238a5
joy9191 <565760450@qq.com>
2022-04-02 17:05:04 +0800

    20220402

--- python练习/07字符串和常用数据结构/字符串反转/reverse01.py
+++ python练习/07字符串和常用数据结构/字符串反转/reverse01.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
 # coding=utf-8
-
+n = input("请输入字符串：")
 a='123456789'
-a = a[::-1]
+
+# 方法1 倒序切片
+a0 = n[::-1]
+print(a0)
+
+# 方法2 反转列表法
+a1=''.join(reversed(n))
+print(a1)
+
+c = list(n)
+c.reverse()
+a2 = ''.join(c)
+print(a2)
+
+# 方法3 遍历索引法
+a3=''
+for i in n:
+	a3 = i + a3
+print(a3)
 
 b='hello world'
 b =b[::3]
-
-print(a)
 print(b)
