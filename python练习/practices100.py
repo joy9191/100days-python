affd7960da664756ec3f1cb6e7e6489aa65e215d
JOY_91 <565760450@qq.com>
2022-05-12 22:47:41 +0800

    20220512

--- python练习/practices100.py
+++ python练习/practices100.py
@@ -21,7 +21,19 @@ def bonus():
 			profit = arr[i]
 	return b
 
+def nums():
+	for i in range(2,85):
+	    if 168 % i == 0:
+	        j = 168 / i;
+	        if  i > j and (i + j) % 2 == 0 and (i - j) % 2 == 0 :
+	            m = (i + j) / 2
+	            n = (i - j) / 2
+	            x = n * n - 100
+	            print(x)
+
+
 
 if __name__ == '__main__':
 	# three_figure()
-	print(bonus())
+	# print(bonus())
+	nums()
